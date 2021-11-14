49a2461ef7f094fcc3ed9926bfb537d279b4c0d7
jose-xd <80435394+jose-xd@users.noreply.github.com>
2021-11-13 22:31:50 -0700

    mongo env

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost/levelsx', {
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.s9tls.mongodb.net/Levelsx?retryWrites=true&w=majority`, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 })
     .then(db => console.log("DB is connected"))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
