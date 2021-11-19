642965268ade0113981e92ed6a3e829d8e61a67a
jose-xd <80435394+jose-xd@users.noreply.github.com>
2021-11-18 22:48:39 -0700

    error correction

--- src/routes/user.js
+++ src/routes/user.js
@@ -46,7 +46,7 @@ router.get('/dashboard/:id', isAuthenticated, async (req, res) => {
     }).then(res => res.json());
     if (!fetchResultBot.user) return res.
         redirect(
-            `https://discord.com/oauth2/authorize?client_id=858543288637718548&scope=bot+applications.commands&permissions=8&guild_id=${urlId}&redirect_uri=http%3A%2F%2Fec2-34-237-242-160.compute-1.amazonaws.com%3A5000%2Fdashboard`
+            `https://discord.com/oauth2/authorize?client_id=858543288637718548&scope=bot+applications.commands&permissions=8&guild_id=${urlId}&redirect_uri=http%3A%2F%2Fec2-34-237-242-160.compute-1.amazonaws.com%2Fdashboard`
         );
 
 
